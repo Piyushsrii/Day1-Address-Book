b4458e6f55efda0bb7175c9cc15ea4c90f10c96c
unknown <nitinmakin124.com>
2020-08-27 21:54:08 +0530

    Added-Adding  new person in address book

---
+++ IAddressBook.java
@@ -0,0 +1,15 @@
+public interface IAddressBook
+{
+    public void add();
+    public void display();
+    /*public void edit(String firstName);
+    public void deletePersonData(String firstName);
+    public void updatePersonDetails(int personId);
+    public void sortByfirstName();
+    public void sortBypincode();
+    public void viewByCityState(String  city , String state);
+    public void searchByCityState(String  city , String state);
+
+    */
+
+}
